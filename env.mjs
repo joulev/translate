5ed936723aec7894f0072654b6b455d673b1476e
Vu Van Dung <me@joulev.dev>
2023-06-20 16:08:04 +0700

    set up rate limiting for /completion
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- env.mjs
+++ env.mjs
@@ -5,8 +5,12 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     OPENAI_API_KEY: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   },
   runtimeEnv: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
 });
